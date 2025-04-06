582d2bfadd3f98a9e57bc7580c40597c8892a374
ZoyidjonNasretdinov <zoyidjonnasretdinovcoder@gmail.con>
2025-04-06 13:49:14 +0500

    add hero image and changed style

--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -57,22 +57,22 @@ const Hero = () => {
         </div>
 
         {/* Right Column */}
-        <div className="lg:w-1/2 relative">
-          
+        <div className="lg:w-3/5 relative">
+          <Image src="/assets/a.png" alt="Hero Image" width={600} height={600}  className='ml-40'/>
           {/* Badges */}
-          <div className="absolute top-8 right-8 bg-white rounded-full shadow-md p-2 flex items-center text-sm text-gray-700">
+          <div className="absolute top-35 right-35 bg-white rounded-full shadow-md p-2 flex items-center text-sm text-gray-700">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 text-green-500" viewBox="0 0 20 20" fill="currentColor">
               <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
             </svg>
             Secure Payment
           </div>
-          <div className="absolute bottom-16 left-8 bg-white rounded-full shadow-md p-2 flex items-center text-sm text-gray-700">
+          <div className="absolute bottom-50 left-50 bg-white rounded-full shadow-md p-2 flex items-center text-sm text-gray-700">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
               <path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 000-2H6z" clipRule="evenodd" />
             </svg>
             Fast Delivery
           </div>
-          <div className="absolute bottom-8 right-0 transform translate-x-1/4 -translate-y-1/4">
+          <div className="absolute bottom-40 right-40 transform translate-x-1/4 -translate-y-1/4">
             <div className="relative">
               <div className="w-20 h-20 rounded-full bg-purple-600 text-white flex items-center justify-center text-sm font-bold">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 mb-1" viewBox="0 0 20 20" fill="currentColor">
