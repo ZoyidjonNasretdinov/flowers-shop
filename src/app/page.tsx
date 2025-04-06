f67bd73da5f5b1a28eba2ec4d9c004d769c91c5e
ZoyidjonNasretdinov <zoyidjonnasretdinovcoder@gmail.con>
2025-04-06 21:35:07 +0500

    FAQs and  news latter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Layout from '@/layout/layout/layout'
 import AboutSection from '@/sections/about-section'
 import DealsOfTheDay from '@/sections/deals-of-the-day'
+import FAQsSection from '@/sections/faqs-section'
+import FollowUsInstagram from '@/sections/follow-us-instagram'
 import Hero from '@/sections/hero'
 import LatestNewsBlogs from '@/sections/latest-new-blogs'
 import NewArrivalProducts from '@/sections/new-arrival-products'
+import NewsletterSubscription from '@/sections/news-latter-subscription'
 import { DealsCarousel, Occasions } from '@/sections/occasions'
 import Testimonials from '@/sections/Testimonials'
 import TopSellerProducts from '@/sections/top-seller-product'
@@ -24,6 +27,11 @@ const Home = () => {
       <DealsOfTheDay />
       <Testimonials />
       <LatestNewsBlogs />
+      <OccasionBar />
+      <FollowUsInstagram />
+      <OccasionBar />
+      <FAQsSection />
+      <NewsletterSubscription />
     </Layout>
   )
 }
