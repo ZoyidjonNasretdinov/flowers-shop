0fcf7bf94ebf9e48951286de1f18b32fdd2effe9
ZoyidjonNasretdinov <zoyidjonnasretdinovcoder@gmail.com>
2025-04-20 13:14:54 +0500

    add contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,15 @@
 import Layout from '@/layout/layout/layout'
 import ContactSection from '@/sections/contact-section'
+import FeaturesSection from '@/sections/features-section'
+import NewsletterSubscription from '@/sections/news-latter-subscription'
 import React from 'react'
 
 const Contact = () => {
   return (
     <Layout>
       <ContactSection />
+      <FeaturesSection />
+      <NewsletterSubscription/>
     </Layout>
   )
 }
